50af0347ed1972d7adf5f1639e847d4f9eb371f7
M. Scott Ford <scott@corgibytes.com>
2020-01-22 22:28:57 -0500

    Read packages.json directly

--- LibMetrics/Languages/Php/ComposerRepository.cs
+++ LibMetrics/Languages/Php/ComposerRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.Json;
 using System.Text.RegularExpressions;
@@ -59,13 +60,55 @@ namespace LibMetrics.Languages.Php
     {
       if (!_packageInfoCache.ContainsKey(name))
       {
-        var client = new RestClient("https://repo.packagist.org/p");
-        var request = new RestRequest($"/{name}.json");
-        var response = client.Execute(request);
+        var packageListing = Request("/packages.json");
 
-        _packageInfoCache[name] = response.Content;
+        using var packageJson = JsonDocument.Parse(packageListing);
+        var urlTemplate = packageJson.RootElement.GetProperty("providers-url").
+          GetString();
+
+        var providerIncludes = packageJson.RootElement.
+          GetProperty("provider-includes").EnumerateObject();
+        foreach (var providerInclude in providerIncludes)
+        {
+          var providerName = providerInclude.Name;
+          var providerHash = providerInclude.Value.GetProperty("sha256").
+            GetString();
+
+          var providerListing =
+            Request($"/{providerName.Replace("%hash%", providerHash)}");
+          using var providerListingJson = JsonDocument.Parse(providerListing);
+          JsonElement packageProvider;
+          if (providerListingJson.RootElement.GetProperty("providers").
+            TryGetProperty(name, out packageProvider))
+          {
+            var packageHash = packageProvider.GetProperty("sha256").GetString();
+            var packageUrl = urlTemplate.
+              Replace("%package%", name).
+              Replace("%hash%", packageHash);
+            var packageDetails = Request($"/{packageUrl}");
+
+            _packageInfoCache[name] = packageDetails;
+            break;
+          }
+        }
       }
       return _packageInfoCache[name];
+    }
+
+    private Dictionary<string, string> _requestCache =
+      new Dictionary<string, string>();
+    private string Request(string url)
+    {
+      if (!_requestCache.ContainsKey(url))
+      {
+        var client = new RestClient(_baseUrl);
+        var request = new RestRequest(url);
+        var response = client.Execute(request);
+
+        _requestCache[url] = response.Content;
+      }
+
+      return _requestCache[url];
     }
 
     private bool IsUnstable(string version)
