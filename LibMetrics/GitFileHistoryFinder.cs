2e25a7f8b43a56120cc8732dc7c7f1eb3a5ace2a
M. Scott Ford <scott@corgibytes.com>
2020-05-20 17:35:25 -0400

    Adds ability to detect if a project path is a cloneable url

--- LibMetrics/GitFileHistoryFinder.cs
+++ LibMetrics/GitFileHistoryFinder.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using LibGit2Sharp;
 
 namespace LibMetrics
@@ -6,7 +10,39 @@ namespace LibMetrics
   {
     public bool DoesPathContainHistorySource(string projectRootPath)
     {
-      return Repository.IsValid(projectRootPath);
+      bool result = Repository.IsValid(projectRootPath);
+      if (!result)
+      {
+        result = IsCloneable(projectRootPath);
+      }
+
+      return result;
+    }
+
+    private bool IsCloneable(string url)
+    {
+      var result = true;
+      var options = new CloneOptions {Checkout = false};
+
+      string tempFolder = Path.Combine(
+        Path.GetTempPath(),
+        Guid.NewGuid().ToString());
+
+      try
+      {
+        Repository.Clone(url, tempFolder, options);
+      }
+      catch (NotFoundException)
+      {
+        result = false;
+      }
+
+      if (Directory.Exists(tempFolder))
+      {
+        Directory.Delete(tempFolder, recursive: true);
+      }
+
+      return result;
     }
 
     public IFileHistory FileHistoryOf(string projectRootPath, string targetFile)
