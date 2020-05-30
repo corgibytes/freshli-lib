a1b91f6a0827a598ad6a70080d53b17831dd3d59
M. Scott Ford <scott@corgibytes.com>
2020-05-30 14:26:07 -0400

    Improvements to use of Approvals

--- LibMetrics.Test/Acceptance.cs
+++ LibMetrics.Test/Acceptance.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 using ApprovalTests;
 using ApprovalTests.Reporters;
 using LibMetrics.Languages.Php;
@@ -21,9 +19,11 @@ namespace LibMetrics.Test
       var runner = new Runner();
 
       var rubyFixturePath = Fixtures.Path("ruby", "nokotest");
-      var results = runner.Run(rubyFixturePath);
+      var results = runner.Run(
+        rubyFixturePath,
+        asOf: new DateTime(2020, 01, 01));
 
-      AssertNokoFixtureRubyGemsResults(results);
+      Approvals.VerifyAll(results, "results");
     }
 
     [Fact]
@@ -35,9 +35,9 @@ namespace LibMetrics.Test
 
       var repoUrl =
         "https://github.com/corgibytes/libmetrics-fixture-ruby-nokotest";
-      var results = runner.Run(repoUrl);
+      var results = runner.Run(repoUrl, asOf: new DateTime(2020, 01, 01));
 
-      AssertNokoFixtureRubyGemsResults(results);
+      Approvals.VerifyAll(results, "results");
     }
 
     [Fact]
@@ -49,40 +49,9 @@ namespace LibMetrics.Test
       var runner = new Runner();
 
       var repoUrl = "https://github.com/feedbin/feedbin";
-      var results = runner.Run(repoUrl);
+      var results = runner.Run(repoUrl, asOf: new DateTime(2020, 01, 01));
 
-      var output = new StringWriter();
-
-      foreach (var resultSet in results) {
-        if (resultSet.Date <= new DateTime(2020, 01, 01)) {
-          output.WriteLine(
-            $"{resultSet.Date.ToString("yyyy/MM/dd")}: " +
-            $"{resultSet.LibYear.Total.ToString("F3")}"
-          );
-        }
-      }
-
-      Approvals.Verify(output);
-    }
-
-    private static void AssertNokoFixtureRubyGemsResults(IList<MetricsResult> results) {
-      // nokotest contains a git repository with the following history:
-      // Gemfile v1 created on 2017/01/01 references nokogiri 1.7.0
-      // Gemfile v2 created on 2018/01/01 references nokogiri 1.8.1
-      // Gemfile v3 created on 2019/01/01 references nokogiri 1.9.1
-
-      var output = new StringWriter();
-
-      foreach (var resultSet in results) {
-          if (resultSet.Date <= new DateTime(2020, 01, 01)) {
-              output.WriteLine(
-                  $"{resultSet.Date.ToString("yyyy/MM/dd")}: " +
-                  $"{resultSet.LibYear.Total.ToString("F3")}"
-              );
-          }
-      }
-
-      Approvals.Verify(output);
+      Approvals.VerifyAll(results, "results");
     }
 
     [Fact]
@@ -97,18 +66,7 @@ namespace LibMetrics.Test
         phpFixturePath,
         asOf: new DateTime(2020, 01, 01));
 
-      var output = new StringWriter();
-
-      foreach (var resultSet in results)
-      {
-        output.WriteLine($"{resultSet.Date.ToString("yyyy/MM/dd")}: " +
-                         $"{resultSet.LibYear.Total.ToString("F3")}");
-      }
-
-      var expected = new StringWriter();
-      expected.WriteLine("2020/01/01: 6.126");
-
-      Assert.Equal(expected.ToString(), output.ToString());
+      Approvals.VerifyAll(results, "results");
     }
 
     [Fact]
@@ -123,18 +81,9 @@ namespace LibMetrics.Test
         phpFixturePath,
         asOf: new DateTime(2020, 01, 01));
 
-      var output = new StringWriter();
+      Approvals.VerifyAll(results, "results");
+    }
 
-      foreach (var resultSet in results)
-      {
-        output.WriteLine($"{resultSet.Date.ToString("yyyy/MM/dd")}: " +
-                         $"{resultSet.LibYear.Total.ToString("F3")}");
-      }
-
-      var expected = new StringWriter();
-      expected.WriteLine("2020/01/01: 3.214");
-
-      Assert.Equal(expected.ToString(), output.ToString());
     }
   }
 }
