910d7278a8313fcbb4e29df0e605e56164898b97
M. Scott Ford <scott@corgibytes.com>
2020-06-01 11:12:52 -0400

    Adds start at a requirements.txt support

---
+++ LibMetrics/Languages/Python/PipRequirementsTxtManifestFinder.cs
@@ -0,0 +1,17 @@
+﻿namespace LibMetrics.Languages.Python {
+  public class PipRequirementsTxtManifestFinder: IManifestFinder {
+    public IFileHistoryFinder FileFinder { get; set; }
+    public bool DoesPathContainManifest(string projectRootPath) {
+      throw new System.NotImplementedException();
+    }
+
+    public string LockFileName { get; }
+    public IPackageRepository RepositoryFor(string projectRootPath) {
+      throw new System.NotImplementedException();
+    }
+
+    public IManifest ManifestFor(string projectRootPath) {
+      throw new System.NotImplementedException();
+    }
+  }
+}
