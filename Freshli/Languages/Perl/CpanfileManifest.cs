925117073843c082b1e1c97d6dff444b136f7e1e
M. Scott Ford <scott@corgibytes.com>
2020-06-21 00:36:08 -0400

    Adds failing test for parsing cpanfile

---
+++ Freshli/Languages/Perl/CpanfileManifest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Freshli.Languages.Perl {
+  public class CpanfileManifest : AbstractManifest {
+    public override void Parse(string contents) {
+      throw new System.NotImplementedException();
+    }
+
+    public override bool UsesExactMatches => false;
+  }
+}
