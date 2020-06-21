4e8a8dad1ced570a1c209bc7a50be09cffeec40c
M. Scott Ford <scott@corgibytes.com>
2020-06-20 23:52:41 -0400

    Extracts abstract base from manifest class for requirements.txt files

---
+++ Freshli/Languages/AbstractManifest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Freshli.Languages {
+  public abstract class AbstractManifest : IManifest {
+    private IDictionary<string, PackageInfo> _packages =
+      new Dictionary<string, PackageInfo>();
+
+    public int Count => _packages.Count;
+    public abstract bool UsesExactMatches { get; }
+
+    public IEnumerator<PackageInfo> GetEnumerator() {
+      return _packages.Values.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() {
+      return GetEnumerator();
+    }
+
+    public void Add(string packageName, string packageVersion) {
+      _packages[packageName] = new PackageInfo(
+        packageName,
+        packageVersion
+      );
+    }
+
+    public abstract void Parse(string contents);
+    public PackageInfo this[string packageName] => _packages[packageName];
+  }
+}
