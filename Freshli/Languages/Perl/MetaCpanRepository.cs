c04484531a5502a8d42e1565475669df9d621682
M. Scott Ford <scott@corgibytes.com>
2020-06-23 23:35:31 -0400

    Adds stubbed out repository

---
+++ Freshli/Languages/Perl/MetaCpanRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Freshli.Languages.Perl {
+  public class MetaCpanRepository: IPackageRepository {
+    public VersionInfo LatestAsOf(DateTime date, string name) {
+      throw new NotImplementedException();
+    }
+
+    public VersionInfo VersionInfo(string name, string version) {
+      throw new NotImplementedException();
+    }
+
+    public VersionInfo Latest(string name, string thatMatches, DateTime asOf) {
+      throw new NotImplementedException();
+    }
+  }
+}
