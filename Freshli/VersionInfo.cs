912f192a533e3bf3332a8cb37b4941d65e7ea5d4
M. Scott Ford <scott@corgibytes.com>
2020-06-28 12:30:38 -0400

    Adds a log message when not parsable

--- Freshli/VersionInfo.cs
+++ Freshli/VersionInfo.cs
@@ -166,6 +166,13 @@ namespace Freshli
         {
           BuildMetadata = buildMetadataValue;
         }
+      } 
+      else 
+      {
+        logger.Log(
+          LogLevel.Warn, 
+          @"Unable to parse version string: {_version}. If you think this " + 
+            @"is an error, then please create an issue on GitHub.");
       }
     }
 
