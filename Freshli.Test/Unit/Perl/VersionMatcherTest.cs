da94fdb1372de364bf0edb365f17adf444b46376
M. Scott Ford <scott@corgibytes.com>
2020-06-28 12:00:30 -0400

    Implement CPAN-based VersionMatcher

---
+++ Freshli.Test/Unit/Perl/VersionMatcherTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Freshli.Languages.Perl;
+using Xunit;
+
+namespace Freshli.Test.Unit.Perl {
+  public class VersionMatcherTest {
+    [Theory]
+    [InlineData("1.0", "1.0", true)]
+    [InlineData("1.1", "1.0", true)]
+    [InlineData("0.9", "1.0", false)]
+    [InlineData("2.0", "1.0", true)]
+    [InlineData("1.1", "1.1", true)]
+    [InlineData("1.2", "1.1", true)]
+    [InlineData("1.0", "1.1", false)]
+    [InlineData("1.1", "> 0.9", true)]
+    [InlineData("0.9", "> 1.1", false)]
+    [InlineData("1.1", ">= 0.9", true)]
+    [InlineData("0.9", ">= 1.1", false)]
+    [InlineData("2.0", "> 2.0", false)]
+    [InlineData("2.0", ">= 2.0", true)]
+    [InlineData("2.0", "< 2.0", false)]
+    [InlineData("2.0", "<= 2.0", true)]
+    [InlineData("1.1", "< 0.9", false)]
+    [InlineData("1.1", "<= 0.9", false)]
+    [InlineData("0.9", "< 1.1", true)]
+    [InlineData("0.9", "<= 1.1", true)]
+    [InlineData("0.9", "== 1.0", false)]
+    [InlineData("1.0", "== 1.0", true)]
+    [InlineData("0.9", "!= 1.0", true)]
+    [InlineData("1.0", "!= 1.0", false)]
+    [InlineData("1.5", ">= 1.2, != 1.5", false)]
+    [InlineData("1.2", ">= 1.2, != 1.5", true)]
+    [InlineData("2.0", ">= 1.2, != 1.5", true)]
+    [InlineData("1.5", ">= 1.2, != 1.5, < 2.0", false)]
+    [InlineData("1.2", ">= 1.2, != 1.5, < 2.0", true)]
+    [InlineData("2.0", ">= 1.2, != 1.5, < 2.0", false)]
+    public void DoesMatchHandleVersionSpecifierOperator(
+      string version, 
+      string expression, 
+      bool doesMatch
+    ) {
+      var info = new VersionInfo() {Version = version};
+      var matcher = VersionMatcher.Create(expression);
+      Assert.Equal(doesMatch, matcher.DoesMatch(info));
+    }
+
+    [Theory]
+    [InlineData("2.0", VersionMatcher.OperationKind.GreaterThanEqual, "2.0")]
+    [InlineData(">= 2.0", VersionMatcher.OperationKind.GreaterThanEqual, "2.0")]
+    [InlineData("== 2.0", VersionMatcher.OperationKind.Matching, "2.0")]
+    [InlineData("<= 2.0", VersionMatcher.OperationKind.LessThanEqual, "2.0")]
+    [InlineData("< 2.0", VersionMatcher.OperationKind.LessThan, "2.0")]
+    [InlineData("> 2.0", VersionMatcher.OperationKind.GreaterThan, "2.0")]
+    [InlineData("!= 2.0", VersionMatcher.OperationKind.NotEqual, "2.0")]
+    [InlineData(">=2.0", VersionMatcher.OperationKind.GreaterThanEqual, "2.0")]
+    [InlineData("==2.0", VersionMatcher.OperationKind.Matching, "2.0")]
+    [InlineData("<=2.0", VersionMatcher.OperationKind.LessThanEqual, "2.0")]
+    [InlineData("<2.0", VersionMatcher.OperationKind.LessThan, "2.0")]
+    [InlineData(">2.0", VersionMatcher.OperationKind.GreaterThan, "2.0")]
+    [InlineData("!=2.0", VersionMatcher.OperationKind.NotEqual, "2.0")]
+    public void IdentifiesOperationAndVersion(
+      string expression,
+      VersionMatcher.OperationKind operation,
+      string version
+    ) {
+      var matcher = VersionMatcher.Create(expression);
+
+      var basicMatcher = (BasicVersionMatcher) matcher;
+      Assert.Equal(operation, basicMatcher.Operation);
+      Assert.Equal(
+        new VersionInfo {Version = version}, 
+        basicMatcher.BaseVersion
+      );
+    }
+  }
+}
---
+++ Freshli/Languages/Perl/VersionMatcher.cs
@@ -0,0 +1,126 @@
+﻿using System.Linq;
+
+namespace Freshli.Languages.Perl {
+  public abstract class VersionMatcher {
+    public enum OperationKind
+    {
+      Matching,
+      LessThan,
+      LessThanEqual,
+      GreaterThan,
+      GreaterThanEqual,
+      NotEqual
+    }
+    
+    public static VersionMatcher Create(string value) {
+      value = value.Trim();
+      
+      if (value.Contains(',')) {
+        var splits = value.Split(',');
+        return new CompoundVersionMatcher(
+          splits.Select(Create).ToArray()
+        );
+      }
+      
+      if (value.StartsWith("==")) {
+        var version = value.Replace("==", "").Trim();
+        return new BasicVersionMatcher {
+          Operation = OperationKind.Matching,
+          BaseVersion = new VersionInfo {Version = version}
+        };
+      }
+      
+      if (value.StartsWith(">=")) {
+        var version = value.Replace(">=", "").Trim();
+        return new BasicVersionMatcher {
+          Operation = OperationKind.GreaterThanEqual,
+          BaseVersion = new VersionInfo {Version = version}
+        };
+      }
+
+      if (value.StartsWith("<=")) {
+        var version = value.Replace("<=", "").Trim();
+        return new BasicVersionMatcher {
+          Operation = OperationKind.LessThanEqual,
+          BaseVersion = new VersionInfo {Version = version}
+        };
+      }
+
+      if (value.StartsWith("<")) {
+        var version = value.Replace("<", "").Trim();
+        return new BasicVersionMatcher {
+          Operation = OperationKind.LessThan,
+          BaseVersion = new VersionInfo {Version = version}
+        };
+      }
+
+      if (value.StartsWith(">")) {
+        var version = value.Replace(">", "").Trim();
+        return new BasicVersionMatcher {
+          Operation = OperationKind.GreaterThan,
+          BaseVersion = new VersionInfo {Version = version}
+        };
+      }
+
+      if (value.StartsWith("!=")) {
+        var version = value.Replace("!=", "").Trim();
+        return new BasicVersionMatcher {
+          Operation = OperationKind.NotEqual,
+          BaseVersion = new VersionInfo {Version = version}
+        };
+      }
+
+      return new BasicVersionMatcher {
+        Operation = OperationKind.GreaterThanEqual,
+        BaseVersion = new VersionInfo {Version = value}
+      };
+    }
+
+    public abstract bool DoesMatch(VersionInfo version);
+  }
+
+  public class BasicVersionMatcher : VersionMatcher {
+    public OperationKind Operation { get; set; }
+    public VersionInfo BaseVersion { get; set; }
+    
+    public override bool DoesMatch(VersionInfo version) {
+      if (Operation == OperationKind.NotEqual) {
+        return version.CompareTo(BaseVersion) != 0;
+      }
+
+      if (Operation == OperationKind.GreaterThan) {
+        return version.CompareTo(BaseVersion) > 0;
+      }
+      
+      if (Operation == OperationKind.GreaterThanEqual) {
+        return version.CompareTo(BaseVersion) >= 0;
+      }
+      
+      if (Operation == OperationKind.LessThan) {
+        return version.CompareTo(BaseVersion) < 0;
+      }
+      
+      if (Operation == OperationKind.LessThanEqual) {
+        return version.CompareTo(BaseVersion) <= 0;
+      }
+
+      if (Operation == OperationKind.Matching) {
+        return version.CompareTo(BaseVersion) == 0;
+      }
+
+      return false;
+    }
+  }
+  
+  public class CompoundVersionMatcher: VersionMatcher {
+    private VersionMatcher[] _matchers;
+      
+    public CompoundVersionMatcher(VersionMatcher[] matchers) {
+      _matchers = matchers;
+    }
+
+    public override bool DoesMatch(VersionInfo version) {
+      return _matchers.All(m => m.DoesMatch(version));
+    }
+  }
+}
