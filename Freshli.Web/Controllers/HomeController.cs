a8c5ca0e3a6c21b6a739f6835e1a4cee397f6747
M. Scott Ford <scott@corgibytes.com>
2020-07-08 21:02:07 -0400

    Wraps long lines

--- Freshli.Web/Controllers/HomeController.cs
+++ Freshli.Web/Controllers/HomeController.cs
@@ -28,10 +28,16 @@ namespace Freshli.Web.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [ResponseCache(
+          Duration = 0,
+          Location = ResponseCacheLocation.None,
+          NoStore = true
+        )]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel {
+              RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
 
         [HttpPost]
--- Freshli.Web/Startup.cs
+++ Freshli.Web/Startup.cs
@@ -20,13 +20,15 @@ namespace Freshli.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add
+        // services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure
+        // the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +38,9 @@ namespace Freshli.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change
+                // this for production scenarios,
+                // see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
