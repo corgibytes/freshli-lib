b58baa5909e97ae6c6833716b34f97f0bc39f4b9
M. Scott Ford <scott@corgibytes.com>
2020-07-14 22:16:09 -0400

    Runs migrations before the website starts

--- docker-entrypoint-web.sh
+++ docker-entrypoint-web.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+set -ex
+
+export PATH="$PATH:/root/.dotnet/tools"
+
 # build and run the website if no arguments
 if [ $# -eq 0 ]; then
     dotnet restore
     dotnet build
-    dotnet run --project=Freshli.Web/Freshli.Web.csproj --no-build --no-launch-profile
+    cd Freshli.Web
+    dotnet ef database update
+    dotnet run --no-build --no-launch-profile
 fi
 
 # allows running commands in the container such as "bash"
