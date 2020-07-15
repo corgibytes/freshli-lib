9967ac897f40ebc8396391c958f865fd0a26c368
M. Scott Ford <scott@corgibytes.com>
2020-07-14 21:51:23 -0400

    Reformats file

--- Freshli.Web/Controllers/HomeController.cs
+++ Freshli.Web/Controllers/HomeController.cs
@@ -7,42 +7,38 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Freshli.Web.Models;
 
-namespace Freshli.Web.Controllers
-{
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+namespace Freshli.Web.Controllers {
+  public class HomeController : Controller {
+    private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(
-          Duration = 0,
-          Location = ResponseCacheLocation.None,
-          NoStore = true
-        )]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {
-              RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-            });
-        }
-
-        [HttpPost]
-        public IActionResult Analysis(AnalysisRequest analysisRequest) {
-          return View(analysisRequest);
-        }
+    public HomeController(ILogger<HomeController> logger) {
+      _logger = logger;
     }
+
+    public IActionResult Index() {
+      return View();
+    }
+
+    public IActionResult Privacy() {
+      return View();
+    }
+
+    [ResponseCache(
+      Duration = 0,
+      Location = ResponseCacheLocation.None,
+      NoStore = true
+    )]
+    public IActionResult Error() {
+      return View(
+        new ErrorViewModel {
+          RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+        }
+      );
+    }
+
+    [HttpPost]
+    public IActionResult Analysis(AnalysisRequest analysisRequest) {
+      return View(analysisRequest);
+    }
+  }
 }
