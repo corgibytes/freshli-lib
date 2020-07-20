7707840fd7243f1e528d53431ea097820952aa3e
nickiemc <nickiem@gmail.com>
2020-07-20 16:39:25 -0500

    Fixes line that was too long :shakes_fist:

--- Freshli/Exceptions/LatestVersionNotFoundException.cs
+++ Freshli/Exceptions/LatestVersionNotFoundException.cs
@@ -4,7 +4,8 @@ namespace Freshli.Exceptions {
   public class LatestVersionNotFoundException : Exception {
 
     public LatestVersionNotFoundException(DateTime date, string dependency)
-      : base($"Unable to find latest version of dependency {dependency} as of {date:d}.")
+      : base($"Unable to find latest version of dependency " +
+        $"{dependency} as of {date:d}.")
     {
     }
 
