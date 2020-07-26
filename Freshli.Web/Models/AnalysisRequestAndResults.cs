11678db3bce8372d4150374d12d57b08ed1de235
M. Scott Ford <scott@corgibytes.com>
2020-07-26 01:07:19 -0400

    Fixes styling

--- Freshli.Web/Models/AnalysisRequestAndResults.cs
+++ Freshli.Web/Models/AnalysisRequestAndResults.cs
@@ -2,10 +2,9 @@ using XPlot.Plotly;
 
 namespace Freshli.Web.Models
 {
-  public class AnalysisRequestAndResults
-  {
+  public class AnalysisRequestAndResults {
     public AnalysisRequest Request { get; set; }
-   public PlotlyChart ProjectTotalLibYearOverTime { get; set; }
-   public PlotlyChart ProjectAverageLibYearOverTime { get; set; }
+    public PlotlyChart ProjectTotalLibYearOverTime { get; set; }
+    public PlotlyChart ProjectAverageLibYearOverTime { get; set; }
   }
 }
