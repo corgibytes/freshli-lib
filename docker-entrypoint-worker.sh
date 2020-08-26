d4c9b5619a29f04741476a740d991be683386601
M. Scott Ford <scott@corgibytes.com>
2020-08-26 09:37:51 -0400

    Adds different startup script for Production env

--- docker-entrypoint-worker.sh
+++ docker-entrypoint-worker.sh
@@ -6,14 +6,18 @@ export PATH="$PATH:/root/.dotnet/tools"
 
 # build and run the website if no arguments
 if [ $# -eq 0 ]; then
-    cd XPlot
-    dotnet tool restore
-    dotnet paket restore
-    cd -
-    cd Freshli.Web.Worker
-    dotnet restore
-    dotnet build --no-restore
-    dotnet run --no-build
+    if [ $ASPNETCORE_ENVIRONMENT != "Production" ]; then
+        cd XPlot
+        dotnet tool restore
+        dotnet paket restore
+        cd -
+        cd Freshli.Web.Worker
+        dotnet restore
+        dotnet build --no-restore
+        dotnet run --no-build
+    else
+        ./Freshli.Web.Worker
+    fi
 fi
 
 # allows running commands in the container such as "bash"
