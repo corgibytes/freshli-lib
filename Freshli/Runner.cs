616b0d6e6ece5d7d0a0d5a04b41b01ad6f98aab6
Daniel Hein <dch1992@gmail.com>
2021-02-02 09:45:38 -0500

    Converted DateTimeHelper to Extensions class
    Changes DateTimeHelper.cs to utilize extension functionality. This makes the class easier to call and read where it is used.

--- Freshli/Runner.cs
+++ Freshli/Runner.cs
@@ -91,7 +91,7 @@ namespace Freshli {
     }
 
     public IList<MetricsResult> Run(string analysisPath) {
-      var asOf = DateTimeHelper.ConvertToEndOfDay(DateTime.Today);
+      var asOf = DateTime.Today.ToEndOfDay();
       return Run(analysisPath, asOf: asOf);
     }
 
