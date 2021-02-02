88c29057a0a90d1df12c54947f509c2d14f65cb7
Dave Farinelli <d@dfar.io>
2021-02-02 11:37:47 -0500

    Runs .csproj files (needs wildcards, needs tests)

---
+++ Freshli/Languages/CSharp/FreshliNuGetVersionInfo.cs
@@ -0,0 +1,31 @@
+using System;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+
+namespace Freshli.Languages.CSharp {
+  /**
+   *  This serves as a wrapper for the 'NuGetVersion' class in the NuGet
+   *  Client SDK.
+   */
+  public class FreshliNuGetVersionInfo : IVersionInfo {
+    private IPackageSearchMetadata _packageSearchMetadata;
+
+    public FreshliNuGetVersionInfo(IPackageSearchMetadata packageSearchMetadata)
+    {
+        _packageSearchMetadata = packageSearchMetadata;
+    }
+
+    public string Version => _packageSearchMetadata.Identity.Version.ToString();
+
+    public DateTime DatePublished =>
+        _packageSearchMetadata.Published.Value.DateTime;
+
+    public bool IsPreRelease =>
+        _packageSearchMetadata.Identity.Version.IsPrerelease;
+
+    public int CompareTo(object obj) {
+      //throw new NotImplementedException();
+      return 0;
+    }
+  }
+}
--- Freshli/Languages/CSharp/NuGetManifestFinder.cs
+++ Freshli/Languages/CSharp/NuGetManifestFinder.cs
@@ -5,11 +5,12 @@ namespace Freshli.Languages.CSharp {
   public class NuGetManifestFinder : IManifestFinder {
     public IFileHistoryFinder FileFinder { get; set; }
 
-    // We have multiple "lock files"
-    public string LockFileName => "";
+    // We have multiple "lock files" that we'll need to address,
+    // but for now just using wildcard .csproj
+    public string LockFileName => "*.csproj";
 
     public bool DoesPathContainManifest(string projectRootPath) {
-      return FileFinder.Exists(projectRootPath, "*.csproj");
+      return FileFinder.Exists(projectRootPath, LockFileName);
     }
 
     public IPackageRepository RepositoryFor(string projectRootPath) {
