8acb9718cd9af8874f06a1d26b87961939260fe3
M. Scott Ford <scott@corgibytes.com>
2021-03-27 23:20:12 -0400

    Switches similar tests from using [Fact] to [Theory]

--- Corgibytes.Freshli.Lib.Test/Unit/LibYearCalculatorTest.cs
+++ Corgibytes.Freshli.Lib.Test/Unit/LibYearCalculatorTest.cs
@@ -1,53 +1,64 @@
 using System;
+using System.Runtime.InteropServices;
 using Xunit;
 
 namespace Corgibytes.Freshli.Lib.Test.Unit {
   public class LibYearCalculatorTest {
-    [Fact]
-    public void Tokenizer() {
-      var oldVersion = BuildVersion("1.1.0", 2017, 04, 07);
-      var newVersion = BuildVersion("1.1.3", 2019, 06, 13);
-
-      Assert.Equal(2.18, LibYearCalculator.Compute(oldVersion, newVersion), 2);
-    }
-
-    [Fact]
-    public void Parser() {
-      var oldVersion = BuildVersion("v3.6.0", 2018, 02, 08);
-      var newVersion = BuildVersion("v4.0.0", 2019, 08, 28);
-
-      Assert.Equal(1.55, LibYearCalculator.Compute(oldVersion, newVersion), 2);
-    }
-
-    [Fact]
-    public void VfsStream() {
-      var oldVersion = BuildVersion("v1.1.4", 2014, 09, 14);
-      var newVersion = BuildVersion("v1.6.8", 2019, 10, 30);
-
-      Assert.Equal(5.13, LibYearCalculator.Compute(oldVersion, newVersion), 2);
-    }
-
-    [Fact]
-    public void Compute() {
-      var olderVersion = BuildVersion("1.7.0", 2016, 12, 27);
-      var newerVersion = BuildVersion("1.7.1", 2017, 03, 20);
-
-      Assert.Equal(
-        0.227,
-        LibYearCalculator.Compute(olderVersion, newerVersion),
-        3
-      );
-    }
-
-    private static SemVerVersionInfo BuildVersion(
-      string rawVersion,
-      int year,
-      int month,
-      int day
+    [Theory]
+    [InlineData(
+      new object[] {"1.1.0", 2017, 04, 07},
+      new object[] {"1.1.3", 2019, 06, 13},
+      2.18,
+      2
+    )]
+    [InlineData(
+      new object[] {"v3.6.0", 2018, 02, 08},
+      new object[] {"v4.0.0", 2019, 08, 28},
+      1.55,
+      2
+    )]
+    [InlineData(
+      new object[] {"v1.1.4", 2014, 09, 14},
+      new object[] {"v1.6.8", 2019, 10, 30},
+      5.13,
+      2
+    )]
+    [InlineData(
+      new object[] {"1.7.0", 2016, 12, 27},
+      new object[] {"1.7.1", 2017, 03, 20},
+      0.227,
+      3
+    )]
+    public void Compute(
+      object[] oldVersion,
+      object[] newVersion,
+      double expectedValue,
+      int precision
     ) {
+
+      var actualValue = LibYearCalculator.Compute(
+        BuildVersion(oldVersion),
+        BuildVersion(newVersion)
+      );
+      Assert.Equal(expectedValue, actualValue, precision);
+    }
+
+    private static SemVerVersionInfo BuildVersion(object[] parts) {
+      var rawVersion = (string) parts[0];
+      var year = (int) parts[1];
+      var month = (int) parts[2];
+      var day = (int) parts[3];
       return new(
         rawVersion,
-        new DateTimeOffset(year, month, day, 00, 00, 00, TimeSpan.Zero)
+        new DateTimeOffset(
+          year,
+          month,
+          day,
+          00,
+          00,
+          00,
+          TimeSpan.Zero
+        )
       );
     }
   }
