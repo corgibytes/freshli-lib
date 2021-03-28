18d840c4fe6face41e408ce7655debfed5487ad3
M. Scott Ford <scott@corgibytes.com>
2021-03-27 23:56:14 -0400

    Migrates repetitive [Fact] tests to use [Theory]

--- Corgibytes.Freshli.Lib.Test/Unit/AnalysisDatesTest.cs
+++ Corgibytes.Freshli.Lib.Test/Unit/AnalysisDatesTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Corgibytes.Freshli.Lib.Util;
 using Moq;
 using Xunit;
@@ -23,160 +24,85 @@ namespace Corgibytes.Freshli.Lib.Test.Unit {
       );
     }
 
-    [Fact]
-    public void DateRange() {
-      ConfigureDateHistoryByYear(year: 2017, count: 3);
-      var stopDate = ParseExact("2020-01-01T23:59:59.9999999Z");
-      var analysisDates = new AnalysisDates(
-        _history.Object,
-        asOf: stopDate
-      );
-
-      var expectedDates = BuildExpectedDates(
-        firstDateString: "2017-01-01T23:59:59.9999999Z",
-        secondDateString: "2017-02-01T00:00:00.0000000Z",
-        stopDate
-      );
-
-      Assert.Equal(expectedDates, analysisDates);
+    [Theory]
+    [InlineData(
+      new[] {2017, 3},
+      "2020-01-01T23:59:59.9999999Z",
+      "2017-01-01T23:59:59.9999999Z",
+      "2017-02-01T00:00:00.0000000Z"
+    )]
+    [InlineData(
+      new[] {2017, 3},
+      "2020-01-01T23:59:59.9999999-08:00",
+      "2017-01-01T23:59:59.9999999Z",
+      "2017-02-01T00:00:00.0000000-08:00"
+    )]
+    [InlineData(
+      new[] {2017, 4},
+      "2019-01-01T23:59:59.9999999Z",
+      "2017-01-01T23:59:59.9999999Z",
+      "2017-02-01T00:00:00.0000000Z"
+    )]
+    public void RangeByYear(
+      int[] historyConfig,
+      string stopDateString,
+      string firstDateString,
+      string secondDateString
+    ) {
+      ConfigureDateHistoryByYear(historyConfig);
+      AssertAnalysisDates(firstDateString, secondDateString, stopDateString);
     }
 
-    [Fact]
-    public void DateRangeUsesTimeZoneFromAsOfValue() {
-      ConfigureDateHistoryByYear(year: 2017, count: 3);
-      var stopDate = ParseExact("2020-01-01T23:59:59.9999999-08:00");
-      var analysisDates = new AnalysisDates(
-        _history.Object,
-        asOf: stopDate
-      );
-
-      var expectedDates = BuildExpectedDates(
-        firstDateString: "2017-01-01T23:59:59.9999999Z",
-        secondDateString: "2017-02-01T00:00:00.0000000-08:00",
-        stopDate
-      );
-
-      Assert.Equal(expectedDates, analysisDates);
-    }
-
-    [Fact]
-    public void DateRangeStartsExactlyAtStartOfMonth() {
-      ConfigureDateHistory(
+    [Theory]
+    [InlineData(
+      new[] {
         "2017-01-01T00:00:00.0000000Z",
         "2018-01-01T23:59:59.9999999Z",
         "2019-01-01T23:59:59.9999999Z"
-      );
-      var stopDate = ParseExact("2020-01-01T23:59:59.9999999Z");
-      var analysisDates = new AnalysisDates(
-        _history.Object,
-        asOf: stopDate
-      );
-
-      var expectedDates = BuildExpectedDates(
-        firstDateString: "2017-01-01T00:00:00.0000000Z",
-        secondDateString: "2017-02-01T00:00:00.0000000Z",
-        stopDate
-      );
-
-      Assert.Equal(expectedDates, analysisDates);
-    }
-
-    [Fact]
-    public void DateRangeStopsOnSpeciedDate() {
-      ConfigureDateHistoryByYear(year: 2017, count: 4);
-      var stopDate = ParseExact("2019-01-01T23:59:59.9999999Z");
-      var analysisDates = new AnalysisDates(
-        _history.Object,
-        asOf: stopDate
-      );
-
-      var expectedDates = BuildExpectedDates(
-        firstDateString: "2017-01-01T23:59:59.9999999Z",
-        secondDateString: "2017-02-01T00:00:00.0000000Z",
-        stopDate
-      );
-
-        Assert.Equal(expectedDates, analysisDates);
-    }
-
-    [Fact]
-    public void FirstFileCreatedBeforeStartOfMonthSingleFileVersion() {
-      ConfigureDateHistory("2016-12-15T00:00:00.0000000Z");
-      var stopDate = ParseExact("2019-01-01T00:00:00.0000000Z");
-      var analysisDates = new AnalysisDates(
-        _history.Object,
-        asOf: stopDate
-      );
-
-      var expectedDates = BuildExpectedDates(
-        firstDateString: "2016-12-15T00:00:00.0000000Z",
-        secondDateString: "2017-01-01T00:00:00.0000000Z",
-        stopDate
-      );
-
-      Assert.Equal(expectedDates, analysisDates);
-    }
-
-    [Fact]
-    public void FirstFileCreatedAfterStartOfMonthMultipleFileVersions() {
-      ConfigureDateHistory(
+      },
+      "2020-01-01T23:59:59.9999999Z",
+      "2017-01-01T00:00:00.0000000Z",
+      "2017-02-01T00:00:00.0000000Z"
+    )]
+    [InlineData(
+      new[] {"2016-12-15T00:00:00.0000000Z"},
+      "2019-01-01T00:00:00.0000000Z",
+      "2016-12-15T00:00:00.0000000Z",
+      "2017-01-01T00:00:00.0000000Z"
+    )]
+    [InlineData(
+      new[] {
         "2016-12-15T00:00:00.0000000Z",
         "2017-12-15T00:00:00.0000000Z"
-      );
-      var stopDate = ParseExact("2019-01-01T00:00:00.0000000Z");
-      var analysisDates = new AnalysisDates(
-        _history.Object,
-        asOf: stopDate
-      );
-
-      var expectedDates = BuildExpectedDates(
-        firstDateString: "2016-12-15T00:00:00.0000000Z",
-        secondDateString: "2017-01-01T00:00:00.0000000Z",
-        stopDate
-      );
-
-      Assert.Equal(expectedDates, analysisDates);
-    }
-
-    [Fact]
-    public void FirstFileCreatedOnStartOfMonthSingleFileVersion() {
-      ConfigureDateHistory("2016-12-01T02:00:00.0000000Z");
-      var stopDate = ParseExact("2019-01-01T00:00:00.0000000Z");
-      var analysisDates = new AnalysisDates(
-        _history.Object,
-        asOf: stopDate
-      );
-
-      var expectedDates = BuildExpectedDates(
-        firstDateString: "2016-12-01T02:00:00.0000000Z",
-        secondDateString: "2017-01-01T00:00:00.0000000Z",
-        stopDate
-      );
-
-      Assert.Equal(expectedDates, analysisDates);
-    }
-
-    [Fact]
-    public void FirstFileCreatedOnStartOfMonthMultipleFileVersions() {
-      ConfigureDateHistory(
+      },
+      "2019-01-01T00:00:00.0000000Z",
+      "2016-12-15T00:00:00.0000000Z",
+      "2017-01-01T00:00:00.0000000Z"
+    )]
+    [InlineData(
+      new[] {"2016-12-01T02:00:00.0000000Z"},
+      "2019-01-01T00:00:00.0000000Z",
+      "2016-12-01T02:00:00.0000000Z",
+      "2017-01-01T00:00:00.0000000Z"
+    )]
+    [InlineData(
+      new[] {
         "2016-12-01T02:00:00.0000000Z",
         "2017-12-01T02:00:00.0000000Z"
-      );
-      var stopDate = ParseExact("2019-01-01T00:00:00.0000000Z");
-      var analysisDates = new AnalysisDates(
-        _history.Object,
-        asOf: stopDate
-      );
-
-      var expectedDates = BuildExpectedDates(
-        firstDateString: "2016-12-01T02:00:00.0000000Z",
-        secondDateString: "2017-01-01T00:00:00.0000000Z",
-        stopDate
-      );
-
-      Assert.Equal(expectedDates, analysisDates);
+      },
+      "2019-01-01T00:00:00.0000000Z",
+      "2016-12-01T02:00:00.0000000Z",
+      "2017-01-01T00:00:00.0000000Z"
+    )]
+    public void Range(
+      string[] historyValues,
+      string stopDateString,
+      string firstDateString,
+      string secondDateString
+    ) {
+      ConfigureDateHistory(historyValues);
+      AssertAnalysisDates(firstDateString, secondDateString, stopDateString);
     }
-
 
     [Fact]
     public void FileDateIsNewerThanAsOfDateAndHistoryOnlyContainsOneVersion() {
@@ -208,6 +134,13 @@ namespace Corgibytes.Freshli.Lib.Test.Unit {
       );
     }
 
+    private void ConfigureDateHistoryByYear(int[] historyConfig) {
+      ConfigureDateHistoryByYear(
+        year: historyConfig[0],
+        count: historyConfig[1]
+      );
+    }
+
     private void ConfigureDateHistoryByYear(int year, int count) {
       var dateStrings = Enumerable.
         Range(year, count).
@@ -230,6 +163,23 @@ namespace Corgibytes.Freshli.Lib.Test.Unit {
       }
 
       return expectedDates;
+    }
+
+    private void AssertAnalysisDates(
+      string firstDateString,
+      string secondDateString,
+      string stopDateString
+    ) {
+      var stopDate = ParseExact(stopDateString);
+      var analysisDates = new AnalysisDates(
+        _history.Object,
+        asOf: stopDate
+      );
+
+      var expectedDates =
+        BuildExpectedDates(firstDateString, secondDateString, stopDate);
+
+      Assert.Equal(expectedDates, analysisDates);
     }
   }
 }
