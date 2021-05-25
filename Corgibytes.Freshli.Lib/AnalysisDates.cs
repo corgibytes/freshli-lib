fb0ab77782e1f88d1086a3e7405b105fbf95708e
Dave Farinelli <dave@corgibytes.com>
2021-05-25 10:59:55 -0400

    Update AnalysisDates.cs

--- Corgibytes.Freshli.Lib/AnalysisDates.cs
+++ Corgibytes.Freshli.Lib/AnalysisDates.cs
@@ -31,9 +31,6 @@ namespace Corgibytes.Freshli.Lib {
         _dates.Add(dayOf);
         date = date.AddMonths(1);
       }
-
-      // include today's date:
-      _dates.Add(DateTime.Today);
     }
 
     public IEnumerator<DateTime> GetEnumerator() {
