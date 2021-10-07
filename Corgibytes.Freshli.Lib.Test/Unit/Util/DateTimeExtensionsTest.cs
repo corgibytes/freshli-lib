5b825c3384fd0689560cee89dcf80f246bafa7e5
M. Scott Ford <scott@corgibytes.com>
2021-10-07 01:53:14 +0000

    Finishes converting date extensions tests

--- Corgibytes.Freshli.Lib.Test/Unit/Util/DateTimeExtensionsTest.cs
+++ Corgibytes.Freshli.Lib.Test/Unit/Util/DateTimeExtensionsTest.cs
@@ -13,53 +13,176 @@ namespace Corgibytes.Freshli.Lib.Test.Unit.Util
         {
             public override DateTimeOffset InvokeMethod(DateTimeOffset value) => value.ToEndOfDay();
 
-            public override TheoryData<DateTimeOffset, DateTimeOffset, int> DataForTestingExtensionMethod => new()
+            public override TheoryData<IList<int>, IList<int>, int> DataForTestingExtensionMethod => new()
             {
                 {
-                    BuildDateTimeOffset(new[] { 2020, 1, 5, 23, 59, 59, 999, 9998 }),
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 }),
+                    new[] { 2020, 1, 5, 23, 59, 59, 999, 9998 },
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
                     -1
                 },
                 {
-                    BuildDateTimeOffset(new[] { 2020, 1, 5, 23, 59, 59, 999, 9999 }),
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 }),
+                    new[] { 2020, 1, 5, 23, 59, 59, 999, 9999 },
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
                     -1
                 },
                 {
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 0, 0, 0, 0, 0 }),
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 }),
+                    new[] { 2020, 1, 6, 0, 0, 0, 0, 0 },
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
                     0
                 },
                 {
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 0, 0, 0, 0, 1 }),
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 }),
+                    new[] { 2020, 1, 6, 0, 0, 0, 0, 1 },
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
                     0
                 },
                 {
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 12, 0, 0, 0, 0 }),
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 }),
+                    new[] { 2020, 1, 6, 12, 0, 0, 0, 0 },
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
                     0
                 },
                 {
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 23, 59, 59, 999, 9998 }),
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 }),
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9998 },
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
                     0
                 },
                 {
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 }),
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 }),
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
                     0
                 },
                 {
-                    BuildDateTimeOffset(new[] { 2020, 1, 7, 0, 0, 0, 0, 0 }),
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 }),
+                    new[] { 2020, 1, 7, 0, 0, 0, 0, 0 },
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
                     1
                 },
                 {
-                    BuildDateTimeOffset(new[] { 2020, 1, 7, 0, 0, 0, 0, 1 }),
-                    BuildDateTimeOffset(new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 }),
+                    new[] { 2020, 1, 7, 0, 0, 0, 0, 1 },
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
                     1
                 },
+            };
+        }
+
+        public class ToStartOfDay : DateModificationExtensionMethodTestFixture<ToStartOfDay>
+        {
+            public override DateTimeOffset InvokeMethod(DateTimeOffset value) => value.ToStartOfDay();
+
+            public override TheoryData<IList<int>, IList<int>, int> DataForTestingExtensionMethod => new()
+            {
+                {
+                    new[] { 2020, 1, 5, 23, 59, 59, 999, 9998 },
+                    new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
+                    -1
+                },
+                {
+                    new[] { 2020, 1, 5, 23, 59, 59, 999, 9999 },
+                    new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
+                    -1
+                },
+                {
+                    new[] { 2020, 1, 6, 0, 0, 0, 0, 0 },
+                    new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 1, 6, 0, 0, 0, 0, 1 },
+                    new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 1, 6, 12, 0, 0, 0, 0 },
+                    new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9998 },
+                    new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
+                    new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 1, 7, 0, 0, 0, 0, 0 },
+                    new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
+                    1
+                },
+                {
+                    new[] { 2020, 1, 7, 0, 0, 0, 0, 1 },
+                    new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
+                    1
+                }
+            };
+        }
+
+        public class ToStartOfMonth : DateModificationExtensionMethodTestFixture<ToStartOfMonth>
+        {
+            public override DateTimeOffset InvokeMethod(DateTimeOffset value) => value.ToStartOfMonth();
+
+            public override TheoryData<IList<int>, IList<int>, int> DataForTestingExtensionMethod => new()
+            {
+                {
+                    new[] { 2019, 12, 05, 23, 59, 59, 999, 9998 },
+                    new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
+                    -1
+                },
+                {
+                    new[] { 2020, 01, 05, 23, 59, 59, 999, 9998 },
+                    new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 01, 05, 23, 59, 59, 999, 9999 },
+                    new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 1, 6, 0, 0, 0, 0, 0 },
+                    new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 1, 6, 0, 0, 0, 0, 1 },
+                    new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 1, 6, 12, 0, 0, 0, 0 },
+                    new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9998 },
+                    new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
+                    new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 1, 7, 0, 0, 0, 0, 0 },
+                    new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 1, 7, 0, 0, 0, 0, 1 },
+                    new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
+                    0
+                },
+                {
+                    new[] { 2020, 2, 7, 0, 0, 0, 0, 0 },
+                    new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
+                    1
+                },
+                {
+                    new[] { 2020, 2, 7, 0, 0, 0, 0, 1 },
+                    new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
+                    1
+                }
             };
         }
 
@@ -67,18 +190,20 @@ namespace Corgibytes.Freshli.Lib.Test.Unit.Util
         {
             public abstract DateTimeOffset InvokeMethod(DateTimeOffset value);
 
-            public abstract TheoryData<DateTimeOffset, DateTimeOffset, int> DataForTestingExtensionMethod { get; }
+            public abstract TheoryData<IList<int>, IList<int>, int> DataForTestingExtensionMethod { get; }
 
             [Theory]
             [InstanceMemberData(nameof(DataForTestingExtensionMethod))]
             public void ExtensionMethod(
-                DateTimeOffset input,
-                DateTimeOffset target,
+                int[] inputDateComponents,
+                int[] targetDateComponents,
                 int comparison)
             {
-                var transformedInput = InvokeMethod(input);
+                var inputDate = BuildDateTimeOffset(inputDateComponents);
+                var targetDate = BuildDateTimeOffset(targetDateComponents);
+                var transformedInputDate = InvokeMethod(inputDate);
 
-                Assert.Equal(comparison, transformedInput.CompareTo(target));
+                Assert.Equal(comparison, transformedInputDate.CompareTo(targetDate));
             }
 
             protected static DateTimeOffset BuildDateTimeOffset(int[] dateArguments)
@@ -95,246 +220,6 @@ namespace Corgibytes.Freshli.Lib.Test.Unit.Util
                 ).AddTicks(dateArguments[7]);
                 return inputDate;
             }
-
-        }
-    }
-
-
-    public class DateTimeExtensionsTest
-    {
-        [Theory]
-        [InlineData(
-          nameof(DateTimeExtensions.ToEndOfDay),
-          new[] { 2020, 1, 5, 23, 59, 59, 999, 9998 },
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
-          -1
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToEndOfDay),
-          new[] { 2020, 1, 5, 23, 59, 59, 999, 9999 },
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
-          -1
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToEndOfDay),
-          new[] { 2020, 1, 6, 0, 0, 0, 0, 0 },
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToEndOfDay),
-          new[] { 2020, 1, 6, 0, 0, 0, 0, 1 },
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToEndOfDay),
-          new[] { 2020, 1, 6, 12, 0, 0, 0, 0 },
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToEndOfDay),
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9998 },
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToEndOfDay),
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToEndOfDay),
-          new[] { 2020, 1, 7, 0, 0, 0, 0, 0 },
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
-          1
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToEndOfDay),
-          new[] { 2020, 1, 7, 0, 0, 0, 0, 1 },
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
-          1
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfDay),
-          new[] { 2020, 1, 5, 23, 59, 59, 999, 9998 },
-          new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
-          -1
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfDay),
-          new[] { 2020, 1, 5, 23, 59, 59, 999, 9999 },
-          new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
-          -1
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfDay),
-          new[] { 2020, 1, 6, 0, 0, 0, 0, 0 },
-          new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfDay),
-          new[] { 2020, 1, 6, 0, 0, 0, 0, 1 },
-          new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfDay),
-          new[] { 2020, 1, 6, 12, 0, 0, 0, 0 },
-          new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfDay),
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9998 },
-          new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfDay),
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
-          new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfDay),
-          new[] { 2020, 1, 7, 0, 0, 0, 0, 0 },
-          new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
-          1
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfDay),
-          new[] { 2020, 1, 7, 0, 0, 0, 0, 1 },
-          new[] { 2020, 1, 6, 00, 00, 00, 000, 0000 },
-          1
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfMonth),
-          new[] { 2019, 12, 05, 23, 59, 59, 999, 9998 },
-          new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
-          -1
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfMonth),
-          new[] { 2020, 01, 05, 23, 59, 59, 999, 9998 },
-          new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfMonth),
-          new[] { 2020, 01, 05, 23, 59, 59, 999, 9999 },
-          new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfMonth),
-          new[] { 2020, 1, 6, 0, 0, 0, 0, 0 },
-          new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfMonth),
-          new[] { 2020, 1, 6, 0, 0, 0, 0, 1 },
-          new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfMonth),
-          new[] { 2020, 1, 6, 12, 0, 0, 0, 0 },
-          new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfMonth),
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9998 },
-          new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfMonth),
-          new[] { 2020, 1, 6, 23, 59, 59, 999, 9999 },
-          new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfMonth),
-          new[] { 2020, 1, 7, 0, 0, 0, 0, 0 },
-          new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfMonth),
-          new[] { 2020, 1, 7, 0, 0, 0, 0, 1 },
-          new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
-          0
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfMonth),
-          new[] { 2020, 2, 7, 0, 0, 0, 0, 0 },
-          new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
-          1
-        )]
-        [InlineData(
-          nameof(DateTimeExtensions.ToStartOfMonth),
-          new[] { 2020, 2, 7, 0, 0, 0, 0, 1 },
-          new[] { 2020, 01, 01, 00, 00, 00, 000, 0000 },
-          1
-        )]
-        public void EndOfDayValuesCompareCorrectly(
-          string methodName,
-          int[] dateArguments,
-          int[] targetDateArguments,
-          int comparison
-        )
-        {
-            AssertDateExtensionMethod(
-              dateArguments,
-              targetDateArguments,
-              comparison,
-              date => InvokeExtensionMethod(date, methodName)
-            );
-        }
-
-        private static DateTimeOffset InvokeExtensionMethod(
-          DateTimeOffset date,
-          string methodName
-        )
-        {
-            return (DateTimeOffset)typeof(DateTimeExtensions).GetMethod(methodName).
-              Invoke(date, new object[] { date });
-        }
-
-        private static DateTimeOffset BuildDateTimeOffset(int[] dateArguments)
-        {
-            var inputDate = new DateTimeOffset(
-              dateArguments[0],
-              dateArguments[1],
-              dateArguments[2],
-              dateArguments[3],
-              dateArguments[4],
-              dateArguments[5],
-              dateArguments[6],
-              TimeSpan.Zero
-            ).AddTicks(dateArguments[7]);
-            return inputDate;
-        }
-
-        private void AssertDateExtensionMethod(
-          int[] dateArguments,
-          int[] targetDateArguments,
-          int comparison,
-          Func<DateTimeOffset, DateTimeOffset> extensionMethod
-        )
-        {
-            var inputDate = BuildDateTimeOffset(dateArguments);
-            var targetDate = BuildDateTimeOffset(targetDateArguments);
-
-            var transformedDate = extensionMethod(inputDate);
-
-            Assert.Equal(comparison, transformedDate.CompareTo(targetDate));
         }
     }
 }
