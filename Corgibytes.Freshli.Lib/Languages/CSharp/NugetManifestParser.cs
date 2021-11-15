4613bb16f020f031716702804f13d0816195fec1
M. Scott Ford <scott@corgibytes.com>
2021-11-15 21:02:32 +0000

    Implements nuget/csproj parser

---
+++ Corgibytes.Freshli.Lib/Languages/CSharp/NugetManifestParser.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace Corgibytes.Freshli.Lib.Languages.CSharp
+{
+    public class NuGetManifestParser: IManifestParser
+    {
+        public bool UsesExactMatches => true;
+
+        public IEnumerable<PackageInfo> Parse(Stream contentsStream)
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(contentsStream);
+
+            var packages = xmlDoc.GetElementsByTagName("PackageReference");
+            foreach (XmlNode package in packages)
+            {
+                yield return new PackageInfo(
+                    package.Attributes[0].Value,
+                    package.Attributes[1].Value
+                );
+            }
+        }
+
+    }
+}
