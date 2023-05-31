1417252feb70e793358fa1d96bf31f6417e9507e
Mike Dougherty <mike@corgibytes.com>
2023-05-31 10:37:04 -0400

    include NuGet packages.config manifest files

---
+++ Corgibytes.Freshli.Lib/Languages/CSharp/PackagesManifest.cs
@@ -0,0 +1,23 @@
+using System.Xml;
+
+namespace Corgibytes.Freshli.Lib.Languages.CSharp;
+
+public class PackagesManifest : AbstractManifest
+{
+    public override void Parse(string contents)
+    {
+        var xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(contents);
+
+        var packages = xmlDoc.GetElementsByTagName("package");
+        foreach (XmlNode package in packages)
+        {
+            Add(
+                package.Attributes![0].Value,
+                package.Attributes[1].Value
+            );
+        }
+    }
+
+    public override bool UsesExactMatches => true;
+}
